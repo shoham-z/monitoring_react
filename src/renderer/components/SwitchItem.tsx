d5365c060b09474430707ed34ea53eac069fd0f4
BuildTools <unconfigured@null.spigotmc.org>
2025-03-20 23:58:25 +0200

    edit menu

--- src/renderer/components/SwitchItem.tsx
+++ src/renderer/components/SwitchItem.tsx
@@ -1,85 +1,64 @@
-/* eslint-disable default-case */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable prettier/prettier */
-import { MouseEvent, useEffect, useState } from "react";
-import { Item, Menu, RightSlot, Separator, useContextMenu } from 'react-contexify';
-import switchImg from '../../img/switch.png'
-import '../styles/SwitchItem.css';
-import 'react-contexify/ReactContexify.css';
+import { useState, MouseEvent } from "react";
+import { Item, Menu, RightSlot, useContextMenu } from "react-contexify";
+import switchImg from "../../img/switch.png";
+import "../styles/SwitchItem.css";
+import "react-contexify/ReactContexify.css";
 
-function SwitchItem(props: {name: string, reachability: any, ip: any}) {
-  const {name, reachability, ip} = props;
-  const MENU_ID = ip;
-
+function SwitchItem(props: { name: string; reachability: any; ip: any }) {
+  const { name, reachability, ip } = props;
+  const MENU_ID = "switch-menu"; // Same ID for all switches to allow only one open at a time
 
   const { show } = useContextMenu({ id: MENU_ID });
-  const [switchName, _setSwitchName] = useState(name);
-
-  const styleclass = reachability === "up" ? "reachable" : "unreachable";
+  const [editMenuOpen, setEditMenuOpen] = useState(false);
 
   const handleItemClick = (event) => {
-
-    // console.log(event)
-    switch (event.id) {
-      case "ping":
-        console.log("ping")
-        console.log(props)
-        break;
-      case "connect":
-        console.log("connect");
-        console.log(props)
-        break;
-      case "edit":
-        console.log("edit")
-        console.log(props)
-        break;
-      case "delete":
-        console.log("delete")
-        console.log(props)
-        break;
+    if (event.id === "edit") {
+      setEditMenuOpen(true); // Open the fullscreen edit menu
     }
+  };
 
-    console.log("HHHHHHHHHHHHHHHHHAAAAAAAAAAAAAAAAAAA")
+  function displayMenu(e: MouseEvent) {
+    e.preventDefault();
+    show({ event: e });
   }
-
-  function displayMenu(e){
-    // put whatever custom logic you need
-    // you can even decide to not display the Menu
-    show({
-      event: e,
-    });
-  }
-
-  const matchShortcutPing = (e: KeyboardEvent) => {
-    return e.ctrlKey && e.key === 'g';
-  }
-
-  const matchShortcutConnect = (e: KeyboardEvent) => {
-    return e.ctrlKey && e.key === 'h';
-  }
-
 
   return (
-    <div className={`switch-item ${  styleclass}`} onContextMenu={displayMenu}>
-      <img src={switchImg} alt="killjoy kirby"/>
-      <p>{switchName}</p>
-      <Menu id={MENU_ID} className="context-menu">
-        <Item id="ping" onClick={handleItemClick} keyMatcher={matchShortcutPing}>
-          Ping <RightSlot>Ctrl G</RightSlot>
-        </Item>
-        <Item id="cut" onClick={handleItemClick} keyMatcher={matchShortcutConnect}>
-          Connect <RightSlot>Ctrl H</RightSlot>
-        </Item>
-        <Item id="edit" onClick={handleItemClick}>
-          Edit
-        </Item>
-        <Item id="delete" onClick={handleItemClick}>
-          Delete
-        </Item>
-      </Menu>
-    </div>
+    <>
+      <div className={`switch-item ${reachability === "up" ? "reachable" : "unreachable"}`} onContextMenu={displayMenu}>
+        <img src={switchImg} alt="Switch" />
+        <p>{name}</p>
+
+        {/* Right-Click Context Menu */}
+        <Menu id={MENU_ID} className="context-menu">
+          <Item id="ping" onClick={handleItemClick}>
+            Ping <RightSlot>Ctrl G</RightSlot>
+          </Item>
+          <Item id="connect" onClick={handleItemClick}>
+            Connect <RightSlot>Ctrl H</RightSlot>
+          </Item>
+          <Item id="edit" onClick={handleItemClick}>
+            Edit
+          </Item>
+          <Item id="delete" onClick={handleItemClick}>
+            Delete
+          </Item>
+        </Menu>
+      </div>
+
+      {/* Fullscreen Edit Menu (Only One Opens at a Time) */}
+      {editMenuOpen && (
+        <div className="fullscreen-modal">
+          <div className="modal-content">
+            <h2>Edit Switch</h2>
+            <button onClick={() => console.log("Rename clicked")}>Rename</button>
+            <button onClick={() => console.log("Change Icon clicked")}>Change Icon</button>
+            <button className="cancel" onClick={() => setEditMenuOpen(false)}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
