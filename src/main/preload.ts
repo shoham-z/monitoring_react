de169461bed06be041638d58848688887a71154e
Shoham Zeharya <shozeh123@gmail.com>
2025-12-08 13:54:38 +0200

    removed unnecessary code and formatted

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,27 +1,8 @@
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 import { MyNotification } from '../main/util';
 
-export type Channels = 'ipc-example';
-
 const electronHandler = {
   ipcRenderer: {
-    sendMessage(channel: Channels, ...args: unknown[]) {
-      ipcRenderer.send(channel, ...args);
-    },
-    on(channel: Channels, func: (...args: unknown[]) => void) {
-      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
-        func(...args);
-      ipcRenderer.on(channel, subscription);
-
-      return () => {
-        ipcRenderer.removeListener(channel, subscription);
-      };
-    },
-    once(channel: Channels, func: (...args: unknown[]) => void) {
-      ipcRenderer.once(channel, (_event, ...args) => func(...args));
-    },
-    // sendPing: (host: any) => ipcRenderer.send('ping-request', host),
-
     sendPing: async (host: string) => ipcRenderer.invoke('ping-request', host),
 
     sendPingVisible: async (host: string) => ipcRenderer.send('ping-request-visible', host),
@@ -30,14 +11,11 @@ const electronHandler = {
 
     connectRemotely: (ip: any) => ipcRenderer.send('connect-remotely', ip),
 
-    getVars: async () =>
-      ipcRenderer.invoke('get-vars'),
+    getVars: async () => ipcRenderer.invoke('get-vars'),
 
-    saveSwitchList: async (switchList: any[]) =>
-      ipcRenderer.invoke('save-switch-list', switchList),
+    saveSwitchList: async (switchList: any[]) => ipcRenderer.invoke('save-switch-list', switchList),
 
-    loadSwitchList: async () =>
-      ipcRenderer.invoke('load-switch-list'),
+    loadSwitchList: async () => ipcRenderer.invoke('load-switch-list'),
 
     appendNotification: async (notification: MyNotification) => ipcRenderer.invoke('append-notification', notification),
 
